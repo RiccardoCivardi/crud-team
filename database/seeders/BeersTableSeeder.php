7ea2ebd04e0223fa7fba219c3011fd34e31d633d
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-16 16:18:37 +0100

    ciao

---
+++ database/seeders/BeersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Beer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BeersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array_beers = file_get_contents('https://api.sampleapis.com/beers/ale');
+        $array_beers = json_decode($array_beers);
+
+        // dump($array_beers);
+
+        foreach ($array_beers as $beer) {
+            $new_beer = new Beer();
+            $new_beer->name = $beer->name;
+            $new_beer->price = $beer->price;
+            $new_beer->rating = $beer->rating;
+            $new_beer->image = $beer->image;
+            $new_beer->slug = Beer::generateSlug($new_beer->name);
+            // dd($new_beer);
+            $new_beer->save();
+        }
+    }
+}
