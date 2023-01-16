9821cbaa824d2bc8a2e1ade976732b9c27d538a7
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-16 16:17:42 +0100

    [add] migration-seeder, to fix price in seeder

---
+++ database/migrations/2023_01_16_142610_create_beers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('slug')->unique();
+            $table->float('price', 5, 2);
+            $table->float('rating', 2, 1)->default(0);
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+};
